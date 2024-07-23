acd0053243e75feff7055d62868260684b55556a
Maxime Balbastre <maxime.balbastre@gmail.com>
2024-07-23 09:43:32 +0200

    modules updates + buttons to change languages + implementation of language changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,41 @@
 // Import our custom CSS
-import '../scss/styles.scss';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
+//import * as bootstrap from '/node_modules/bootstrap';
 
-import avatar from "../../media/photo_cv2.jpg";
+console.log("js loading");
 
-console.log(process.cwd);
+// Écouteur d'événements pour le sélecteur de langue
+document.getElementById('language_switcher').addEventListener('change', function() {
+    var language = this.value;
+    loadLanguageFile(language);
+    localStorage.setItem('preferredLanguage', language);
+});
+
+// Fonction pour charger le fichier de langue
+function loadLanguageFile(language) {
+    fetch('language/' + language + '.json')
+        .then(response => response.json())
+        .then(data => updateContent(data))
+        .catch(error => console.error('Error loading language file:', error));
+}
+
+// Fonction pour mettre à jour le contenu de la page
+function updateContent(data) {
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            const element = document.getElementById(key);
+            if (element) {
+                element.textContent = data[key];
+            }
+        }
+    }
+}
+
+// Charger la langue préférée à la première visite
+document.addEventListener('DOMContentLoaded', function() {
+    var preferredLanguage = localStorage.getItem('preferredLanguage') || 'fr';
+    loadLanguageFile(preferredLanguage);
+    document.getElementById('language_switcher').value = preferredLanguage;
+});
