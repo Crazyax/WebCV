e6703895d0ddb4cb4aeb8bc9b66558ac28568dc6
Maxime Balbastre <maxime.balbastre@gmail.com>
2024-07-24 11:34:38 +0200

    implementation of change language button and logic

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,50 @@
 console.log("js loading");
 
 // Écouteur d'événements pour le sélecteur de langue
-document.getElementById('language_switcher').addEventListener('change', function() {
-    var language = this.value;
-    loadLanguageFile(language);
-    localStorage.setItem('preferredLanguage', language);
+document.addEventListener('DOMContentLoaded', () => {
+    const selected_language = document.querySelector('#language_selected');
+    const selected_flag = document.querySelector('#selected_flag');
+    const items = document.querySelector('#language_dropdown');
+    const options = items.querySelectorAll('div');
+
+    selected_language.addEventListener('click'  , (e) => {
+        e.stopPropagation();
+        console.log("dropdown clicked");
+        items.classList.toggle("select_hide");
+
+        options.forEach(option => {
+            if (option.getAttribute('language_value') == selected_language.getAttribute('language_value')){
+                option.classList.add('select_hide');
+            }
+            else if (option.classList.contains('select_hide')){
+                option.classList.remove('select_hide');
+            }
+        })
+        
+    })
+
+    options.forEach(option => {
+        option.addEventListener('click', (e) => {
+            e.stopPropagation();
+            console.log("some language selected");
+            selected_flag.innerHTML = option.innerHTML;
+            selected_language.setAttribute('language_value', option.getAttribute('language_value'));
+            items.classList.add('select_hide');
+
+            var language = option.getAttribute('language_value');
+            loadLanguageFile(language);
+            localStorage.setItem('preferredLanguage', language);
+        })
+    })
+    
+    document.addEventListener('click', (e) => {
+        e.stopPropagation();
+        if (!e.target.matches('#language_selected')) {
+            console.log("clicked outside dropdown");
+            items.classList.add('select_hide');
+        }
+    });
+    
 });
 
 // Fonction pour charger le fichier de langue
@@ -32,10 +72,3 @@ function updateContent(data) {
         }
     }
 }
-
-// Charger la langue préférée à la première visite
-document.addEventListener('DOMContentLoaded', function() {
-    var preferredLanguage = localStorage.getItem('preferredLanguage') || 'fr';
-    loadLanguageFile(preferredLanguage);
-    document.getElementById('language_switcher').value = preferredLanguage;
-});
